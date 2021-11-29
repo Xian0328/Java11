e1c45c569bdea015c3b47665037d4fe2d4fe8f35
Xian0328 <91442683+Xian0328@users.noreply.github.com>
2021-11-29 11:55:13 +0800

    Add files via upload

--- class11.java
+++ class11.java
@@ -26,14 +26,30 @@ class CStu implements Data,Test
 	{
 		System.out.println("學號:"+id);
 		System.out.println("姓名:"+id);
-		
+	}
+	public void showScore()
+	{
+		System.out.println("期中成績:"+mid);
+		System.out.println("期末成績:"+finl);
+		System.out.println("平時成績:"+common);
+	}
+	public double calcu()
+	{
+		return mid*0.3+finl*0.3+common*0.4;
+	}
+	public void show()
+	{
+		this.showData();
+		this.showScore();
+		System.out.println("學期成績:"+calcu());
 	}
 }
 public class class11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		CStu stu=new CStu("940001","Fiona",90,92,85);
+		stu.show();
 	}
 
 }
